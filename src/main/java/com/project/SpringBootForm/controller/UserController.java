3a045edf6c91082a7f802cc5714eed9a6e8731f6
ChatcharitCh <chatcharit.choocherd@gmail.com>
2023-07-02 21:22:26 +0700

    Create Register Form

--- src/main/java/com/project/SpringBootForm/controller/UserController.java
+++ src/main/java/com/project/SpringBootForm/controller/UserController.java
@@ -1,4 +1,3 @@
-
 package com.project.SpringBootForm.controller;
 
 import com.project.SpringBootForm.entity.User;
@@ -7,19 +6,24 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
-    
+
     @GetMapping("/register")
     public String showForm(Model model) {
         User user = new User();
         model.addAttribute("user", user);
-        
+
         // Drop down
         List<String> testList = Arrays.asList("test1", "test2", "test3");
         model.addAttribute("testList", testList);
-        
+
         return "register_form";
     }
+
+    
+
 }
